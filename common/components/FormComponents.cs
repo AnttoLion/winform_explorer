a50ec45a2f709060c5ba13ef0f9b9fb6a59a4601
AnttoLion <anttolion@gmail.com>
2023-06-05 17:48:33 -0400

    6/52023 updated-2

--- common/components/FormComponents.cs
+++ common/components/FormComponents.cs
@@ -26,7 +26,6 @@ namespace mjc_dev.common.components
             label.Size = new System.Drawing.Size(150, 31);
 
             textBox = new TextBox();
-            textBox.Location = new Point(50, 50);
             textBox.Size = new Size(300, 20);
             textBox.Font = new Font("Segoe UI Semibold", 15.75F);
         }
@@ -48,8 +47,151 @@ namespace mjc_dev.common.components
         }
     }
 
+    public class FDateTime
+    {
+        private DateTimePicker calendar;
+        private Label label;
+
+        public FDateTime(string labeltext)
+        {
+            label = new Label();
+            label.Text = labeltext;
+
+            label.AutoSize = true;
+            label.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            label.BackColor = System.Drawing.Color.Transparent;
+            label.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F);
+            label.ForeColor = System.Drawing.Color.WhiteSmoke;
+            label.Size = new System.Drawing.Size(150, 31);
+
+            calendar = new DateTimePicker();
+            calendar.Size = new Size(300, 20);
+            calendar.Font = new Font("Arial", 15.75F);
+            calendar.Format = DateTimePickerFormat.Custom;
+            calendar.CustomFormat = "MMMd, yyyy";
+        }
+
+        public DateTimePicker GetDateTimePicker()
+        {
+            return calendar;
+        }
+
+        public Label GetLabel()
+        {
+            return label;
+        }
+
+        public void SetPosition(Point location)
+        {
+            label.Location = location;
+            calendar.Location = new Point(location.X + 200, location.Y);
+        }
+    }
+
+    public class FComboBox
+    {
+        private ComboBox comboBox;
+        private Label label;
+
+        public FComboBox(string labeltext)
+        {
+            label = new Label();
+            label.Text = labeltext;
+
+            label.AutoSize = true;
+            label.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            label.BackColor = System.Drawing.Color.Transparent;
+            label.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F);
+            label.ForeColor = System.Drawing.Color.WhiteSmoke;
+            label.Size = new System.Drawing.Size(150, 31);
+
+            comboBox = new ComboBox();
+            comboBox.Size = new Size(300, 20);
+            comboBox.Font = new Font("Segoe UI Semibold", 15.75F);
+        }
+
+        public ComboBox GetComboBox()
+        {
+            return comboBox;
+        }
+
+        public Label GetLabel()
+        {
+            return label;
+        }
+
+        public void SetPosition(Point location)
+        {
+            label.Location = location;
+            comboBox.Location = new Point(location.X + 200, location.Y);
+        }
+    }
+
     public class FCheckBox
     {
-        public FCheckBox() { }
+        private CheckBox checkBox;
+        private Label label;
+        public FCheckBox(string labeltext)
+        {
+            checkBox = new CheckBox();
+            //checkBox.Location = new Point(50, 50);
+            checkBox.AutoSize = true;
+            checkBox.Font = new Font("Segoe UI Semibold", 15.75F);
+
+            label = new Label();
+            label.Text = labeltext;
+
+            label.AutoSize = true;
+            label.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            label.BackColor = System.Drawing.Color.Transparent;
+            label.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F);
+            label.ForeColor = System.Drawing.Color.WhiteSmoke;
+            label.AutoSize = true;
+            label.AutoSize = true;
+        }
+
+        public CheckBox GetCheckBox()
+        {
+            return checkBox;
+        }
+
+        public Label GetLabel()
+        {
+            return label;
+        }
+
+        public void SetPosition(Point location)
+        {
+            checkBox.Location = new Point(location.X + 10, location.Y + 10);
+            label.Location = new Point(location.X + 30, location.Y);
+        }
+    }
+
+    public class FGroupLabel
+    {
+        private Label label;
+        public FGroupLabel(string labeltext)
+        {
+            label = new Label();
+            label.Text = labeltext;
+
+            label.AutoSize = true;
+            label.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            label.BackColor = System.Drawing.Color.Transparent;
+            label.Font = new System.Drawing.Font("Segoe UI Semibold", 16.75F);
+            label.ForeColor = System.Drawing.Color.DarkGray;
+            label.AutoSize = true;
+            label.AutoSize = true;
+        }
+
+        public Label GetLabel()
+        {
+            return label;
+        }
+
+        public void SetPosition(Point location)
+        {
+            label.Location = location;
+        }
     }
 }
--- forms/SKUList.cs
+++ forms/SKUList.cs
@@ -9,12 +9,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using mjc_dev.model;
+using mjc_dev.forms.modals;
 
 namespace mjc_dev.forms
 {
     public partial class SKUList : GlobalLayout
     {
-
         private HotkeyButton hkAdds = new HotkeyButton("Ins", "Adds", Keys.Insert);
         private HotkeyButton hkDeletes = new HotkeyButton("Del", "Deletes", Keys.Delete);
         private HotkeyButton hkSelects = new HotkeyButton("Enter", "Selects", Keys.Enter);
@@ -25,6 +26,10 @@ namespace mjc_dev.forms
         private HotkeyButton hkProfileHistory = new HotkeyButton("F6", "SKU History", Keys.F6);
         private HotkeyButton hkArchivedSKUs = new HotkeyButton("F8", "Archived SKUs", Keys.F8);
 
+        private GridViewOrigin SKUListGrid = new GridViewOrigin();
+        private DataGridView SKUGridRefer;
+        private DashboardModel model = new DashboardModel();
+
         public SKUList() : base("SKU List", "Select a held order to open")
         {
             InitializeComponent();
@@ -32,12 +37,99 @@ namespace mjc_dev.forms
 
             HotkeyButton[] hkButtons = new HotkeyButton[9] { hkAdds, hkDeletes, hkSelects, hkCrossRefLookup, hkView, hkAdjustQty, hkSKUHistory, hkProfileHistory, hkArchivedSKUs };
             _initializeHKButtons(hkButtons);
-            _addComingSoon();
+            AddHotKeyEvents();
 
-            foreach (HotkeyButton button in hkButtons)
+            InitPriceTierGrid();
+        }
+
+        private void AddHotKeyEvents()
+        {
+            hkAdds.GetButton().Click += (sender, e) =>
             {
-                button.GetButton().Click += new EventHandler(_hotkeyTest);
+                SkuDetail detailModal = new SkuDetail();
+                if (detailModal.ShowDialog() == DialogResult.OK)
+                {
+                    LoadSKUList();
+                }
+            };
+            hkDeletes.GetButton().Click += (sender, e) =>
+            {
+                int selectedSKUId = 0;
+                if (SKUGridRefer.SelectedRows.Count > 0)
+                {
+                    foreach (DataGridViewRow row in SKUGridRefer.SelectedRows)
+                    {
+                        selectedSKUId = (int)row.Cells[0].Value;
+                    }
+                }
+                bool refreshData = model.DeleteSKU(selectedSKUId);
+                if (refreshData)
+                {
+                    LoadSKUList();
+                }
+            };
+            hkSelects.GetButton().Click += (sender, e) =>
+            {
+                updateSKU();
+            };
+        }
+
+        private void InitPriceTierGrid()
+        {
+            SKUGridRefer = SKUListGrid.GetGrid();
+            SKUGridRefer.Location = new Point(0, 95);
+            SKUGridRefer.Width = this.Width;
+            SKUGridRefer.Height = this.Height - 295;
+            this.Controls.Add(SKUGridRefer);
+            this.SKUGridRefer.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.SKUGridView_CellDoubleClick);
+
+            LoadSKUList();
+        }
+
+
+        private void LoadSKUList()
+        {
+            string filter = "";
+            var refreshData = model.LoadSKUData(filter);
+            if (refreshData)
+            {
+                SKUGridRefer.DataSource = model.SKUDataList;
+                SKUGridRefer.Columns[0].Visible = false;
+                SKUGridRefer.Columns[1].HeaderText = "SKU#";
+                SKUGridRefer.Columns[1].Width = 300;
+                SKUGridRefer.Columns[2].HeaderText = "Category";
+                SKUGridRefer.Columns[2].Width = 300;
+                SKUGridRefer.Columns[3].HeaderText = "Description";
+                SKUGridRefer.Columns[3].Width = 500;
+                SKUGridRefer.Columns[4].HeaderText = "Qty Avail";
+                SKUGridRefer.Columns[4].Width = 300;
+                SKUGridRefer.Columns[5].HeaderText = "Qty Tracking";
+                SKUGridRefer.Columns[5].Width = 300;
             }
+        }
+
+        private void updateSKU()
+        {
+            SkuDetail detailModal = new SkuDetail(); ;
+
+            int rowIndex = SKUGridRefer.CurrentCell.RowIndex;
+
+            DataGridViewRow row = SKUGridRefer.Rows[rowIndex];
+            int skuId = (int)row.Cells[0].Value;
+
+            List<dynamic> skuData = new List<dynamic>();
+            skuData = model.GetSKUData(skuId);
+            detailModal.setDetails(skuData, skuData[0].id);
+
+            if (detailModal.ShowDialog() == DialogResult.OK)
+            {
+                LoadSKUList();
+            }
+        }
+
+        private void SKUGridView_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            updateSKU();
         }
     }
 }
